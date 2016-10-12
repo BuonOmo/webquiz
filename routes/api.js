bd4b60de7151d0f78761c8fdfa8f83967c66ea5f
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-10-12 13:22:08 -0400

    Fix #11 and add code 500 for internal error

--- routes/api.js
+++ routes/api.js
@@ -1,61 +1,98 @@
 var express = require('express');
+var fs = require('fs');
 var router = express.Router();
 
-
-router.get('/greetings', function(req, res, next) {
-  res.json({
-    hello:   'world'
-  });
+/**
+ * GET /question/random/:domains   /ask/rand/:domains
+ * Route to get any question in specific domains (domains must be comma
+ * separated)
+ * Response format : {
+ *   question: String,
+ *   answer: Array[String]
+ * }
+ */
+router.get(["/question/random/(:domains)?", "/ask/rand/(:domains)?"], function(req, res, next) {
+  if (req.params.domains) {
+    var domains = req.params.domains.split(',');
+    var ansList = dbAnswers.filter(function (element) {
+      return domains.indexOf(element.domain) != -1;
+    });
+  }
+  else {
+    ansList = dbAnswers;
+  }
+  var index = Math.floor(Math.random()*ansList.length);
+  var ans = ansList[index];
+  if (ans)
+    res.json({
+      domain: ans.domain,
+      question: ans.question,
+      answer: ans.answer
+    });
+  else {
+    res.statusCode = 500;
+    res.json({
+      error: 'No question in these domains.'
+    });
+  }
 });
 
 
 /**
- * GET /question/:id   /ask/:id
- * Route to get question
+ * GET /question/id/:id   /ask/id/:id
+ * Route to get a specific question using its ID
+ * Response format : {
+ *   domain: String,
+ *   question: String,
+ *   answer: Array[String]
+ * }
  */
-router.get(/\/(question|ask)\/:id/, function(req, res, next) {
-  res.json(dbAnswers[req.params.id]['question'] || {
-    error: 'bad id'
-  });
+router.get(["/question/id/:id", "/ask/id/:id"], function(req, res, next) {
+  var ans = dbAnswers[req.params.id]
+  if (ans)
+    res.json({
+      domain: ans.domain,
+      question: ans.question,
+      answer: ans.answer
+    });
+  else {
+    res.statusCode = 500;
+    res.json({
+      error: 'Bad ID.'
+    });
+  }
 });
 
-
+/**
+ * GET /answers/:id/:answer   /answer/:id/:answer   /ans/:id/:answer
+ * Route to get answer
+ * Response format : {
+ *   goodAnswerIndex: Integer,
+ *   isGoodAnswer: Boolean
+ * }
+ */
 router.get('/ans(wers?)?/:id/:answer', function(req, res, next) {
   if (dbAnswers[req.params.id])
     res.json({
       goodAnswerIndex: dbAnswers[req.params.id]['goodAnswer'],
       isGoodAnswer: dbAnswers[req.params.id]['goodAnswer'] == req.params.answer
     });
-  else
+  else {
+    res.statusCode = 500;
     res.json({
       error: 'bad id'
     });
+  }
 });
 
-// TODO: do not send goodAnswer
-dbAnswers = [{
-  id: 0,
-  question: {
-    question: "Comment vérifiez l'intégrité d'un script inséré à l'aide de la valise <script src=\"..\"> ?",
-    answers: [
-      "En utilisant l’attribut \"integrity\"",
-      "On lit le code et on regarde si il est joli",
-      "Je demande l’appel à un ami",
-    ]
-  },
-  goodAnswer: 0
-}, {
-  id: 1,
-  question: {
-    question: "Est-il possible d'utiliser des variables globales en CSS3 ?",
-    answers: [
-      "Oui",
-      "Peut-être",
-      "Non",
-      "Tabarnak, le web c'est pas fait pour moi !",
-    ]
-  },
-  goodAnswer: 0
-}];
+/*
+help from :
+http://stackoverflow.com/questions/10011011/using-node-js-how-do-i-read-a-json-object-into-server-memory#10011078
+*/
+var dbAnswers;
+fs.readFile('database/answers.json','utf8', function (err, data) {
+  if (err) throw err;
+  dbAnswers = JSON.parse(data);
+});
 
 module.exports = router;
