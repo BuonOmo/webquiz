cab1ebbb32a2a0cbf186082dae4e08992002fe98
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-10-12 21:26:09 -0400

    On #12, finish quick test logic

--- public/javascripts/test.js
+++ public/javascripts/test.js
@@ -9,35 +9,38 @@
   var id = document.currentScript.getAttribute('data-id');
 
   var questionData;
-
-  $.get("/api/ask/rand/")
-    .done(function(data) {
-      questionData = data;
-      console.log(questionData);
-      $('#domain').html(questionData.domain);
-      $('#question').html(questionData.question);
-      var answers = $('#answers');
-      for (var answer of questionData.answers) {
-        answers
-          // clone body of an answer
-          .find('.answer:first')
-          .clone()
-          // append current answer elements to the clone
-          .find('input')
-            .attr('name', answer)
-          .end()
-          .find('label')
-            .attr('for', answer)
-            .html(answer)
-          .end()
-          // append the clone to DOM (#answer)
-          .appendTo(answers)
-          // show the clone
-          .show();
-      }
-    })
-    .fail(function (data) {
-      console.log(data);
-    });
-
+  function changeQuestion() {
+    $.get("/api/ask/rand/")
+      .done(function(data) {
+        questionData = data;
+        $('#domain').html(questionData.domain);
+        $('#question').html(questionData.question);
+        var answers = $('#answers');
+        // clean everything except first answer (bones)
+        answers.find('.answer:not(:first)').remove()
+        for (var answer of questionData.answers) {
+          answers
+            // clone body of an answer
+            .find('.answer:first')
+              .clone()
+              // append current answer elements to the clone
+              .find('input')
+                .attr('name', answer)
+              .end()
+              .find('label')
+                .attr('for', answer)
+                .html(answer)
+              .end()
+              // append the clone to DOM (#answer)
+              .appendTo(answers)
+              // show the clone
+              .show();
+        }
+      })
+      .fail(function (data) {
+        console.log(data);
+      });
+    }
+    $(document).ready(changeQuestion);
+    $('#next-question').click(changeQuestion);
 })();
