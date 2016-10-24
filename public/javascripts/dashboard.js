7a99034e6b573103490c82939c4bf865d54b3552
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-10-24 15:21:38 -0400

    Add dynamic statistics on dashboard

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -28,13 +28,37 @@
     if(getLocal('results') == null) {
       $('.exam-statistics').html('Aucune statistique pour le moment');
     } else {
-      var examGrade = 0;
+      var mean = 0;
+      var table = $('tbody');
+      for (var result of getLocal('results')) {
+        // console.log(table.find(''))
+        // avoid dividing by 0
+        var currentGrade = result.goodAnswers / (result.totalAnswers || 1);
+        mean += currentGrade;
+        var date = new Date(result.timestamp);
+        var dateFormat = date.getFullYear()        + "/"  +
+                         pad(date.getMonth()+1, 2) + "/"  +
+                         pad(date.getDate(),    2) + ", " +
+                         pad(date.getHours(),   2) + ":"  +
+                         pad(date.getMinutes(), 2);
+        // comportement here is similar to the one of changeQuestion in test.js
+        table.find('tr:first').clone()
+             .find('.domains').html(result.domains.join(', ') || "tous").end()
+             .find('.date').html(dateFormat).end()
+             .find('.total-answers').html(result.totalAnswers).end()
+             .find('.good-answers').html(result.goodAnswers).end()
+             .find('.grade').html((currentGrade*20).toFixed(1)).end()
+             .appendTo(table).show();
+      }
+      mean /= getLocal('results').length;
+      $('.exam-grade').html((mean*20).toFixed(1) + "/20");
     }
   }
 
   // reset statistics
   $('#reset').click(function () {
     removeLocal('questionStatistics');
+    removeLocal('results');
     printStatistics();
   });
 
--- public/javascripts/utils.js
+++ public/javascripts/utils.js
@@ -100,6 +100,17 @@ function removeAllStorage() {
 }
 
 /**
+ * Function for number padding, it add zeros before the string.
+ * @param  {object} str An object with a toString() method
+ * @param  {number} max
+ * @return {string}
+ */
+function pad (str, max) {
+  str = str.toString();
+  return str.length < max ? pad("0" + str, max) : str;
+}
+
+/**
  * Redirect to a page using it’s path, without basename. This function is made
  * for quick change in basename and logic centralisation.
  * @param  string    path
