3e1ba998f481178ef3f2849092fe64eacee7bf11
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-11-25 17:40:55 -0500

    add answers manipulation (delete and add)

--- public/app/question/question.component.ts
+++ public/app/question/question.component.ts
@@ -10,8 +10,22 @@ export class QuestionComponent implements OnInit {
   question: Question;
   
   ngOnInit(): void {
+    this.question = new Question;
+    this.question.answers = new Array(2);
   }
   
   constructor() {};
   
+  debug(): void {
+    console.log(this.question);
+  }
+  
+  deleteAnswer(index: number): void {
+    if (this.question.answers.length > 2)
+      this.question.answers.splice(index, 1);
+  }
+  
+  addAnswer(): void {
+    this.question.answers.push("");
+  }
 }
