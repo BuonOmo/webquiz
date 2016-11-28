cf56b1a0dea14b8d1a81b13caca2cc2537df27b4
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-11-28 15:05:55 -0500

    remove jqueryFix since we’re not using jquery anymore

--- misc/utils.js
+++ misc/utils.js
@@ -8,17 +8,5 @@ module.exports = {
         data: data
       });
     }
-  },
-  jqueryFix: (toParse) => {
-    /* HACK: this is an ugly fix for a converting issue from jquery. If we sent
-     * the data as a js object, the conversion to json went poorly on numbers and
-     * objects. There should be a proper way to do so, however this wont be fix
-     * because it is a jquery issue, out of our context.
-     */
-    try {
-      return JSON.parse(Object.keys(toParse)[0]);
-    } catch (e) {
-      return toParse;
-    }
   }
 }
