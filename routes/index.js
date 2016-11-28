98a08f417efe3f8244711b5443606b3fef82a65e
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-11-28 18:16:32 -0500

    update routing to avoid not found error

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-
-router.get('/templates/:template', function(req, res, next) {
-  console.log('log me bitch');
-  res.render('templates/' + req.params.template + ".pug");
-});
-
-router.get('/layouts/:layout', function(req, res, next) {
-  res.render('layouts/' + req.params.layout + "/" + req.params.layout + ".pug");
+router.get(/^\/(?!api|less-css)(\w*).*/, function(req, res) {
+  var title = "Webquiz";
+  if (typeof req.params[0] === "string" && req.params[0].length > 0)
+    title+=" — "+req.params[0][0].toUpperCase() + req.params[0].slice(1);
+  res.render('index', { title: title});
 });
 
 module.exports = router;
