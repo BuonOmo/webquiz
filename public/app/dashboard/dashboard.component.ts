b53f86f909a4293e954e521e5095cd0c110deaea
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-11-29 16:15:21 -0500

    add 'taux de reussite' to dashboard statistics

--- public/app/dashboard/dashboard.component.ts
+++ public/app/dashboard/dashboard.component.ts
@@ -1,15 +1,26 @@
 import {Component, OnInit} from '@angular/core';
+import {UserService} from "../user/user.service";
+import {User} from "../user/user";
 
 @Component({
   templateUrl: `/app/dashboard/dashboard.component.html`
 })
 
 export class DashboardComponent implements OnInit{
-
+  totalRatio: number;
+  hasTotalRatio: boolean;
+  
   ngOnInit(): void {
+    this.userService.getUser()
+      .then(user => this.calculateStatistics(user));
+  }
+    
+  calculateStatistics(user: User) {
+    this.totalRatio = Math.round(user.goodAnswers / user.answers * 100);
+    this.hasTotalRatio = !Number.isNaN(this.totalRatio);
   }
 
-  constructor() {};
+  constructor(private userService: UserService) {};
 
 
 }
