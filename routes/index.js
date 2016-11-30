e0d39f337b9d0a5056d1c8292c7f56c362d15be1
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-11-30 13:00:52 -0500

    add component html route

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
+router.get("/component/:component", function(req, res) {
+  res.sendfile("public/app/"+req.params.component+"/"+req.params.component+".component.html");
+});
+
 router.get(/^\/(?!api|less-css)(\w*).*/, function(req, res) {
   var title = "Webquiz";
   if (typeof req.params[0] === "string" && req.params[0].length > 0)
