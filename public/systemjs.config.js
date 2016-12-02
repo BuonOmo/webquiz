348007764273c2df14084137397ef6a00416eb1e
Ulysse Buonomo <buonomo.ulysse@gmail.com>
2016-12-02 17:49:24 -0500

    remove angular-2-modal dependencies

--- public/systemjs.config.js
+++ public/systemjs.config.js
@@ -24,9 +24,7 @@
       '@angular/upgrade': 'npm:@angular/upgrade/bundles/upgrade.umd.js',
       // other libraries
       'rxjs':                      'npm:rxjs',
-      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js',
-      'angular2-modal': 'npm:angular2-modal',
-      'angular2-modal/plugins/bootstrap':  'npm:angular2-modal/bundles'
+      'angular-in-memory-web-api': 'npm:angular-in-memory-web-api/bundles/in-memory-web-api.umd.js'
     },
     // packages tells the System loader how to load when no filename and/or no extension
     packages: {
@@ -36,14 +34,6 @@
       },
       rxjs: {
         defaultExtension: 'js'
-      },
-      'angular2-modal': {
-        defaultExtension: 'js',
-        main: './bundles/angular2-modal.umd'
-      },
-      'angular2-modal/plugins/bootstrap': {
-        defaultExtension: 'js',
-        main: 'angular2-modal.bootstrap.umd'
       }
     }
   });
